16f192ca6294abe5f94795ed763ad66e0792bb29
Vui Nguyen <vui_engineer@yahoo.com>
2017-07-13 11:34:37 -0600

    server and local PDF render functions

--- PDFJSTest/ViewController.swift
+++ PDFJSTest/ViewController.swift
@@ -11,7 +11,6 @@ import WebKit
 
 class ViewController: UIViewController, WKUIDelegate {
 
-  //@IBOutlet weak var webView: UIWebView!
   var webView: WKWebView!
 
   override func loadView() {
@@ -23,44 +22,46 @@ class ViewController: UIViewController, WKUIDelegate {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
 
-    // can we just make this a local URL (ie on my machine?)
-    // or better yet, make it a URL to an opentextbook
-    //let myURL = URL(string: "https://www.apple.com")
-
-    //let myURL = URL(string: "http://localhost:8888")
-    let urlString = "http://localhost:8888/web/viewer.html?file=%2Fexamples%2Fhelloworld%2FLinear%20Regression%20Using%20R-%20An%20Introduction%20to%20Data%20Modeling.pdf"
-
-    //let urlString = "Linear Regression Using R- An Introduction to Data Modeling"
-    /*
-    guard let filePath = Bundle.main.path(forResource: urlString, ofType: "pdf")
-      else {
-        print("error getting filePath")
-        return
-    }
- */
-
-    //let myURL = URL(string: "http://localhost:8888/examples/helloworld/")
-    if let myURL = URL(string: urlString) {
-    //if let myURL = URL(fileURLWithPath: filePath) {
-    do {
-    let myRequest = URLRequest(url: myURL)
-    //webView.loadRequest(myRequest)  // to use with UIWebView
-
-    //let data =  try Data(contentsOf: myURL)
-    //webView.load(data, mimeType: "application/pdf", characterEncodingName:"", baseURL: myURL.deletingLastPathComponent())
-
-
-    webView.load(myRequest)
-    }
-
-    }
+    renderLocalPDF()
+    //renderServerPDF()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+
+  func renderLocalPDF() {
+    let urlString = "Linear Regression Using R- An Introduction to Data Modeling"
+
+    if let filePath = Bundle.main.path(forResource: urlString, ofType: "pdf") {
+      do {
+        let myURL = URL(fileURLWithPath: filePath)
+
+        let myRequest = URLRequest(url: myURL)
+        //webView.loadRequest(myRequest)  // to use with UIWebView
+
+        //let data =  try Data(contentsOf: myURL)
+        //webView.load(data, mimeType: "application/pdf", characterEncodingName:"", baseURL: myURL.deletingLastPathComponent())
+
+        
+        webView.load(myRequest)
+      }
+      
+    }
+  }
+
+  func renderServerPDF() {
+    let urlString = "http://localhost:8888/web/viewer.html?file=%2Fexamples%2Fhelloworld%2FLinear%20Regression%20Using%20R-%20An%20Introduction%20to%20Data%20Modeling.pdf"
+
+    if let myURL = URL(string: urlString) {
+
+      let myRequest = URLRequest(url: myURL)
+
+      webView.load(myRequest)
+    }
+
   }
 
 
